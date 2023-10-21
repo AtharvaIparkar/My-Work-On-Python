a9295d8c13024df095c7af301f1475cfd7d9b7f2
AtharvaIparkar <143648007+AtharvaIparkar@users.noreply.github.com>
2023-10-21 13:51:12 +0530

    Create second.py

---
+++ second.py
@@ -0,0 +1,35 @@
+PRINT THE NAME REVERSE
+n="ATHARVA"
+print(n[::-1])
+
+FANCY OR FORMAT STRING
+n="ATHARVA"
+a="17"
+print(f"my name is {n}")
+print(f"my age is {a}")
+
+MULTIPLE OUTPUT BUT ONE PRINT STATEMENT
+a="Atharva"
+b="Bipin"
+c="Iparkar"
+d="17"
+e="Nashik"
+print(f"First name = {a}\n Middle name = {b}\n Last name = {c}\n Age = {d}\n Address = {e}")
+
+PRINT STATEMENT IN A LINE
+a=10
+b=20
+print(f"the value of a = {a} and the value of b = {b}")
+print(f"value of {a=} and the value {b=}")
+
+SWAPPING THE NUMBERS
+a=10
+b=20
+a,b=b,a
+print(a,b)
+
+ADDING 5 while swapping
+a=10
+b=20
+a,b=b,a+5
+print(a,b)
