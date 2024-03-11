cf270a242cce7ac2071e1a9689ecd9757b963896
AtharvaIparkar <143648007+AtharvaIparkar@users.noreply.github.com>
2024-03-11 17:27:47 +0530

    Update Eight.py

--- Eight.py
+++ Eight.py
@@ -62,6 +62,32 @@ def add(*a):
     print(sum)
 add(1,2,3,4,5)
 
+# Displaying first letter of input and also in capital using a function
+def format_name(name):
+    d = ""
+    s = name.split()
+    for part in s:
+        d += part[0].upper( ) + '.'
+    return d[:-1]
+print(format_name("raam sharma"))
+
+# To convert no. taken as input in form of string into words using a function
+def alpha_num(n):
+    d = {0:"Zero",1:"One",2:"Two",3:"Three",4:"Four",5:"Five",6:"Six",7:"Seven",8:"Eight",9:"Nine",10:"Ten"}
+    for i in n:
+        print(d[int(i)])
+alpha_num("0123")
+
+#Reversing a string using a function
+def w(n):
+    x=" "
+    for i in range(len(n)-1,-1,-1):
+        x+=n[i]
+    print(x,end=" ") 
+w("Hello")
+
+    
+
 
 
 
